114280c86f7b2dee44292ad24e783506561f26e2
unknown <kukuh.baiquni@gmail.com>
2019-05-16 13:54:25 +0700

    fix landing page

--- src/Screen/index.js
+++ src/Screen/index.js
@@ -4,18 +4,28 @@ import { Col, Card, CardHeader, CardBody, Container, Jumbotron } from 'reactstra
 import './Style.scss';
 
 class WelcomeScreen extends Component {
-    render() {
+    componentDidMount() {
         console.log(this.props);
+        if (this.props.login.adminData === null) {
+            localStorage.removeItem('token');
+            this.props.history.replace('/login');
+        }
+    };
+
+    render() {
         return(
-            <div>
-                <Col xs="6">
-                    <Card className="dark-body">
-                        <CardBody>
-                            <h5 style={{color: '#1890ff'}}>Welcome {this.props.login.adminData.name}</h5>
-                            <p className="lead">Your access level is {this.props.login.adminData.status}</p>
-                        </CardBody>
-                    </Card>
-                </Col>
+            <div animated="fadeIn">
+                {
+                    this.props.login.adminData !== null &&
+                    <Col xs="6">
+                        <Card className="dark-body">
+                            <CardBody>
+                                <h5 style={{color: '#1890ff'}}>Welcome {this.props.login.adminData.name}</h5>
+                                <p className="lead">Your access level is {this.props.login.adminData.status}</p>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                }
             </div>
         )
     }
